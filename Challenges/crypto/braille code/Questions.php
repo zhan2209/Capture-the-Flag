77d56f774343ec21b02c5f112d84f9665e206ea8
Gloria Zhang <gloria.zhang@InfiniteCampus.com>
2016-07-13 14:33:45 -0500

    new challenge

---
+++ Challenges/crypto/braille code/Questions.php
@@ -0,0 +1,136 @@
+<?php
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Hints: Braille Language");
+?>
+
+<HTML>
+<body>
+<h1> Q: Why don't blind people skydive? </h1>
+	<a href = "JavaScript:newPopup('/doc/flag.txt');">
+		<img src= "/img/for_blind.jpg"; >
+	</a>
+	<p>
+		<a href="JavaScript:newPopup('/doc/Answer.txt');">
+			Click here find Answer
+		</a>
+	</p>
+	<!-- <script>
+		function showAnswer(){
+			// function to show flag
+		}
+	</script>
+	-->
+</body>
+</HTML>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<script type="text/javascript"> 
+		function newPopup(url) {
+			popupWindow = window.open(
+				url,'popUpWindow','height=100,width=500,top = 500, left = 1000, resizable=yes,menubar=no,location=no,directories=no,status=yes')
+			}
+	</script>
+
+	 
