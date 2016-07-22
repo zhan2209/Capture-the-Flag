efa044fd26d63d386ffd7ca6405e004703c94463
Rob Denman <rob.denman@InfiniteCampus.com>
2016-07-22 15:46:23 -0500

    fixed xss permissions

--- vm_setup/prod/change_permissions.sh
+++ vm_setup/prod/change_permissions.sh
@@ -21,6 +21,8 @@ sudo chmod ugo=x /var/www
 sudo chmod ugo=x /var
 sudo chmod ugo-r /var
 sudo chmod ugo-w /var
+# for xss challenge! may need to add more of these special permissions
+sudo chmod a=rwx /var/www/html/xss/cookie-stealer/comments.json
 
 sudo sed -i 's/D:ALL/D:ALL, !\/usr, !\/bin, \/bin\/ls/g' /etc/sudoers.d/vagrant
 
@@ -30,13 +32,13 @@ sudo sed -i 's/Bits 1024/Bits 2048/g' /etc/ssh/sshd_config
 sudo sed -i 's/GraceTime 120/GraceTime 30/g' /etc/ssh/sshd_config
 sudo sed -i 's/RootLogin without-password/RootLogin no/g' /etc/ssh/sshd_config
 
-# apache server hardening
+# apache server hardening WORKING!
 sudo sed -i 's/#<Directory \/srv\/>/\<Directory \/var\/www\/html>/g' /etc/apache2/apache2.conf
 sudo sed -i 's/#\tOptions Indexes FollowSymLinks/\tOptions -Indexes/g' /etc/apache2/apache2.conf
 sudo sed -i 's/#\tAllowOverride None/<\/Directory>/g' /etc/apache2/apache2.conf
 sudo sed -i 's/#\tRequire all granted/ServerSignature Off/g' /etc/apache2/apache2.conf
 sudo sed -i 's/#<\/Directory>/ServerTokens Prod/g' /etc/apache2/apache2.conf
 
-# change passwords
+# change passwords WORKING!
 echo "root:iNfC16s3c" | sudo chpasswd
 echo "vagrant:1mPoS$18L3" | sudo chpasswd
