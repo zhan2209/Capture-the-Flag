1660e051839bffc469ffecaf8abbe162879616f7
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-02 10:58:08 -0500

    fixed injection2 login bug

--- Challenges/sql-injection/injection2/html/loggedin.html.php
+++ Challenges/sql-injection/injection2/html/loggedin.html.php
@@ -3,21 +3,25 @@
   <head><title>Logged In</title>
   <link rel="stylesheet" type="text/css" href="style.css"></head>
   <body>
-    <?php session_start(); 
-	$info = $_SESSION['userinfo']; ?>
-	<h1>.</h1>
-<div class="stand"><div class="outer-screen"><div class="inner-screen"><div class="form">
-	<h1 id='log'>Login successful!</h1>
-	<p>You're logged in as '<?php echo $info['username']; ?>'</p>
-	<p>Your authorization level is <?php echo $info['authorization']; ?></p>
-	<?php if($info['authorization'] == 9) {
-	  $FLAG = file_get_contents("/var/www/hid/sql-injection/injection2/flag.txt");
-	  echo "<p>The flag is: $FLAG</p>";
+  	<h1>.</h1>
+<?php session_start(); 
+	$info = $_SESSION['userinfo'];
+	if(isset($_SESSION['login']) && $_SESSION['login'] == true) {
+		echo "<div class='stand'><div class='outer-screen'><div class='inner-screen'><div class='form'>";
+		echo "<h1 id='log'>Login successful!</h1>";
+		echo "<p>You're logged in as '".$info['username']."'</p>";
+		echo "<p>Your authorization level is ".$info['authorization']."</p>";
+		if($info['authorization'] == 9) {
+	 		$FLAG = file_get_contents("/var/www/hid/sql-injection/injection2/flag.txt");
+	  		echo "<p>The flag is: $FLAG</p>";
+		} else {
+			echo "<p>You need authorization level 9 to access the flag.<p>";
+		} 
+		echo "<a href='.'>Go back</a>";
+		echo "</div></div></div></div>";
+	} else {
+		header('Location: loginfailed.html.php');
 	}
-	else {
-		echo "<p>You need authorization level 9 to access the flag.<p>";
-	} ?>
-	<a href='.'>Go back</a>
-</div></div></div></div>
+?>
   </body>
 </html>
