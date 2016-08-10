c68141d2f3b9883243b0e0f61df2656ec962b80a
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-10 16:07:22 -0500

    changes to guess style and src

--- Challenges/web/guessing-game/html/index.php
+++ Challenges/web/guessing-game/html/index.php
@@ -1,4 +1,12 @@
 <!doctype html>
+<?php
+	if(session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+	if(!isset($_SESSION['num_guesses'])) {
+		$_SESSION['num_guesses'] = 0;
+	}
+?>
 <html>
 <head>
 <title>Guessing Game</title>
@@ -6,20 +14,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
-<div id='sidebar'><p id="source"><span>Source</span></p></div>
+<div id='sidebar'><?php if($_SESSION['num_guesses'] >= 4) echo "<p id='source'><span>Source</span></p>";?></div>
 <div class='container'>
 <h1>Let's Play a Guessing Game!</h1>
-	<p id='desc'>Guess the password below. <span id='hint'>HINT</span>
-	<span id='hinttext'>Use your keyboard to type your guess, then use your mouse to click the guess button.</span></p>
+	<p id='desc'>Guess the password below.</p>
 	<div id='form'>
-	<form action='#' method='GET'>
+	<form action='' method='GET'>
 		<input type='text' name='attempt'>
 		<input type='submit' value='Guess!'>
 	</form>	
 	</div>
 	<?php
-		$src = file_get_contents("./source.txt");
-		$file = "/var/www/hid/web/guessing-game/secret-password.txt";
+		$src = file_get_contents('source.txt');
+		$file = '../hid/secret-password.txt';
 		$guesses_file = "./guesses.json";
 		$guesses_json = json_decode(file_get_contents($guesses_file), true);
 		extract($_GET);
@@ -27,17 +34,23 @@
 			$password = trim(file_get_contents($file));
 			if($attempt === $password) {
 				echo "<p class='outcome'>How did you figure out the secret password?!</p>";
-				$flag = trim(file_get_contents("/var/www/hid/web/guessing-game/flag.txt"));
+				$flag = trim(file_get_contents('../hid/flag.txt'));
 				echo "<p class='outcome'>The flag is <b>$flag</b></p>";
 			} else {
-				echo "<p class='outcome'><b>Nope!</b> The secret password is not $attempt...</p>";
-				if($attempt) { array_push($guesses_json["guesses"], $attempt); }
-				file_put_contents($guesses_file, json_encode($guesses_json, true));
+				if(trim($attempt)) {
+					echo "<p class='outcome'><b>Nope!</b> The secret password is not $attempt...</p>";
+					array_push($guesses_json["guesses"], trim($attempt));
+					file_put_contents($guesses_file, json_encode($guesses_json, true));
+					$_SESSION['num_guesses']++;
+				} else {
+					echo "<p class='outcome'>We don't want no empty strings</p>";
+				}
 			}
 		}
 	?>
 <div id='prev-guesses'>
-	<p>Previous guesses:</p>
+	<p>Total guesses: <?php echo $_SESSION['num_guesses']; ?> </p>
+	<p>Last five guesses:</p>
 	<?php
 		$guess_arr = [];
 		foreach($guesses_json["guesses"] as $index=>$guess) {
@@ -59,11 +72,18 @@
 		}
 	?>
 </div>
-<div id='popup'>
-	<xmp>
-	<?php echo $src; ?>
-	</xmp>
-</div>
+<span id='hint'>HINT</span>
+<?php if($_SESSION['num_guesses'] >= 5) {
+echo "<span id='hinttext'>Use your keyboard to type your guess, then use your mouse to click the guess button.</span>";
+echo "<div id='popup'>";
+echo "	<xmp>";
+echo $src;
+echo "	</xmp>";
+echo "</div>";
+} else {
+	echo "<span id='hinttext'>Available after 5 guesses.";
+}
+?>
 </div>
 <script>
 $("#source span").hover(function() {
@@ -72,7 +92,7 @@ $("#source span").hover(function() {
 	$('#popup').css({'display' : 'none'});
 });
 $(document).on('click', '#hint', function() {
-	$('#hinttext').css({'color' : 'white'});
+	$('#hinttext').css({'display' : 'inline'});
 });
 </script>
 </body>
