ce46fee5c63cba83b36af42f034d8176389ddf64
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-11 16:08:55 -0500

    fixed inj3 db

--- Challenges/sql-injection/injection3/database/inj3.sql
+++ Challenges/sql-injection/injection3/database/inj3.sql
@@ -14,3 +14,41 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ultra_secret_users`
+--
+
+DROP TABLE IF EXISTS `ultra_secret_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ultra_secret_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text,
+  `password` text,
+  `authorization` int(11) DEFAULT NULL,
+  `email` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ultra_secret_users`
+--
+
+LOCK TABLES `ultra_secret_users` WRITE;
+/*!40000 ALTER TABLE `ultra_secret_users` DISABLE KEYS */;
+INSERT INTO `ultra_secret_users` VALUES (1,'admin','bjBUX3QzaF9mMUBHIQ==',9,'admin@realemail.net'),(2,'jonny_snow','d2ludGVyaXNjb21pbmc=',6,'jonstarkgaryen@the.wall'),(3,'bob_loblaw','YXJyZXN0ZWRkZXZlbG9wbWVudA==',2,'theresalways@moneyinthe.bananastand'),(4,'eric_cartman','bm9raXR0eXRoYXRzbXlwb3RwaWU=',8,'authority@respect.my'),(5,'walter_white','bWV0aG9kbWFu',5,'knocks@iamthe.onewho'),(6,'the_dark_knight','YmF0bW9iaWxl',7,'brucewayne@isnot.me');
+/*!40000 ALTER TABLE `ultra_secret_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-08-11 21:08:12
