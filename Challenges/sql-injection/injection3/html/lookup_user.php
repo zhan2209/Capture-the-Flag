763c03de6e9d9367fc7abbf570797362846d109d
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-19 09:54:01 -0500

    style changes to inj3

--- Challenges/sql-injection/injection3/html/lookup_user.php
+++ Challenges/sql-injection/injection3/html/lookup_user.php
@@ -7,14 +7,15 @@
 
 	$query = "SELECT * FROM ${table_prefix}users WHERE id=$id";
 	$result = mysqli_query($connection, $query);
-
-	if (mysqli_num_rows($result) !== 1) {
-	  die("<p>Could not find user.</p><a href='.'>Go back</a>");
-	}
-
 	$row = mysqli_fetch_array($result);
+	
 	echo "<div class='info-cont'>";
 	echo "<div class='infobox'>";
+	
+	if (mysqli_num_rows($result) !== 1) {
+	  die("<p class='err'>Could not find user.</p><a id='e-back' class='link' href='.'>Go back</a></div></div>");
+	}
+
 	echo "<div id='textline'><p class='left'>User info for id:</p><p class='right'>".htmlspecialchars($row['id'])."</p></div>";
 	echo "<div id='textline'><p class='left'>Username:</p><p class='right'>".htmlspecialchars($row['username'])."</p></div>";
 	echo "<div id='textline'><p class='left'>Email:</p><p class='right'>".htmlspecialchars($row['email'])."</p></div>";
