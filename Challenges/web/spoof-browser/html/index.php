f7de533e055f58f1faca570fe705bb772a36b63c
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-22 15:27:07 -0500

    style changes to spoof

--- Challenges/web/spoof-browser/html/index.php
+++ Challenges/web/spoof-browser/html/index.php
@@ -6,11 +6,11 @@
 </head>
 <body>
 	<div class="center">
-<?php if ($_SERVER['HTTP_USER_AGENT'] == "HACKED"): ?>
+<?php if($_SERVER['HTTP_USER_AGENT'] == "HACKED"): ?>
 		<div class="flag">Congratulations. The flag is flag{i-control-my-browser}.</div>
 <?php else: ?>
-		<p>Change your browser to: <strong>HACKED</strong></p>
-		<p class="hint">Hint: your browser is: <strong><?= $_SERVER['HTTP_USER_AGENT'] ?></strong></p>
+		<p style="font-size:50px">Change your browser to: <strong>HACKED</strong></p>
+		<p class="hint" style="display:none"><span style="color:black">Hint: your browser is </span><strong><?= $_SERVER['HTTP_USER_AGENT'] ?></strong></p>
 <?php endif; ?>
 	</div>
 </body>
