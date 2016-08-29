a72cfea90e1f0543391dcbd27212e37e4a408cfb
Rob Denman <rob.denman@InfiniteCampus.com>
2016-08-29 11:46:23 -0500

    added source code for registration challenge

--- Challenges/sql-injection/registration/html/index.php
+++ Challenges/sql-injection/registration/html/index.php
@@ -27,6 +27,7 @@
 					<input type="submit" name="action" value="Login" class="btn btn-primary">
 				</form>
 			</div>
+			<a href='./login-source.html'>Source</a>
 			<hr>
 			<div class="panel-heading">
 				<h3 class="panel-title">Register</h3>
@@ -40,6 +41,7 @@
 					<input type="submit" name="action" value="Register" class="btn btn-primary">
 				</form>
 			</div>
+			<a href='./register-source.html'>Source</a>
 		</div>
 	</div>
 </body>
